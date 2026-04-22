119133432bd7c0c70dd09517682fdd386843f19d
lzybluee <lzybluee@gmail.com>
2026-04-23 06:43:45 +0800

    Update script

--- run_trad.bat
+++ run_trad.bat
@@ -1,1 +1,16 @@
+python -m pip list | find "matplotlib"
+if %errorlevel% neq 0 (
+    python -m pip install matplotlib
+)
+
+python -m pip list | find "pyperclip"
+if %errorlevel% neq 0 (
+    python -m pip install pyperclip
+)
+
+python -m pip list | find "pillow"
+if %errorlevel% neq 0 (
+    python -m pip install pillow
+)
+
 python main.py trad.txt
